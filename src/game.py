f9cb1948e223f1d6d3518926e869d20b07deb630
BlazejKrzyzanek <bl.krzyzanek@gmail.com>
2019-11-23 13:09:09 +0100

    Signs detection

--- src/game.py
+++ src/game.py
@@ -6,13 +6,13 @@ from skimage.filters.rank import mean_bilateral
 from scipy import ndimage as ndi
 
 
-
 def s_curve(x):
     return 0.5 + (3 * (x - 0.5)) / 2 * np.sqrt(1 + 9 * (x - 0.5) ** 2)
 
 
 def find_game_state(img):
     state = []
+
     # TODO should return game state as simple list - example output :
     #  [['x', '', 'o'], ['', 'x', 'x'], ['o', '', 'o']]
     #  assuming that 'board' parameter is square image with only one game board
@@ -25,31 +25,91 @@ def find_game_state(img):
 
     cdstP = np.copy(cv.cvtColor(dst, cv.COLOR_GRAY2BGR))
 
-    linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 20, None, 35, 3)
-
-    if linesP is not None:
-        for x in range(0, len(linesP)):
-            l = linesP[x][0]
-            cv.line(cdstP, (l[0], l[1]), (l[2], l[3]),
-                    (0, np.random.randint(127, 255), np.random.randint(127, 255)), 2,
-                    cv.LINE_AA)
+    # linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 20, None, 35, 3)
+    #
+    # if linesP is not None:
+    #     for x in range(0, len(linesP)):
+    #         l = linesP[x][0]
+    #         cv.line(cdstP, (l[0], l[1]), (l[2], l[3]),
+    #                 (0, np.random.randint(127, 255), np.random.randint(127, 255)), 2,
+    #                 cv.LINE_AA)
 
     img = ski.img_as_float(img)
     src = ski.img_as_float(src)
     cdstP = ski.img_as_float(cdstP)
 
-    # squares = ndi.binary_fill_holes(dst)
-    # squares = mp.erosion(squares, mp.disk(3))
-    # squares = mp.remove_small_objects(squares, 45)
-    # squares = ski.img_as_ubyte(squares)
-    #
-    # contours, hierarchy = cv.findContours(squares, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    #
-    # for c in contours:
-    #     peri = cv.arcLength(c, True)
-    #     approx = cv.approxPolyDP(c, 0.05 * peri, True)
-    #
-    #     if len(approx) == 4:
-    #         cv.drawContours(dst, approx, -1, (255, 0, 0), 5)
+    squares = ski.img_as_bool(dst)
+    squares = mp.remove_small_holes(squares, 120)
+    squares = mp.dilation(squares, mp.disk(1))
+    squares = mp.thin(squares)
+    squares = ndi.binary_fill_holes(squares)
+    squares = mp.erosion(squares, mp.disk(3))
+    squares = mp.remove_small_objects(squares, 45)
+    squares = ski.img_as_ubyte(squares)
 
-    return [src, dst, cdstP]
+    contours, hierarchy = cv.findContours(squares, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        peri = cv.arcLength(c, True)
+        approx = cv.approxPolyDP(c, 0.05 * peri, True)
+
+        x0, x1, x2, x3, x4, x5 = 15, 35, 37, 57, 60, 80
+        if len(approx) == 4:
+            fields = [dst[x0:x1, x0:x1], dst[x0:x1, x2:x3], dst[x0:x1, x4:x5],
+                      dst[x2:x3, x0:x1], dst[x2:x3, x2:x3], dst[x2:x3, x4:x5],
+                      dst[x4:x5, x0:x1], dst[x4:x5, x2:x3], dst[x4:x5, x4:x5]]
+
+            for i, field in enumerate(fields):
+                field = ski.img_as_ubyte(field)
+                contours, hierarchy = cv.findContours(field, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+                fields[i] = cv.cvtColor(field, cv.COLOR_GRAY2BGR)
+
+                # Remove edges
+                for c in contours:
+                    peri = cv.arcLength(c, True)
+                    approx = cv.approxPolyDP(c, 0.02 * peri, True)
+
+                    if len(approx) <= 4:
+                        cv.fillPoly(fields[i], [c], (0, 0, 0))
+
+                m = np.mean(field[5:-5, 5:-5]) / 255
+                if m < 0.1:
+                    state.append(' ')
+                    continue
+
+                field_filled = ndi.binary_fill_holes(field)
+                field_filled = ski.img_as_ubyte(field_filled)
+                contours, hierarchy = cv.findContours(field_filled, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+                circle = False
+
+                for c in contours:
+                    peri = cv.arcLength(c, True)
+                    approx = cv.approxPolyDP(c, 0.05 * peri, True)
+
+                    area = cv.contourArea(c)
+                    perimeter = cv.arcLength(c, False)
+                    if perimeter == 0.0:
+                        continue
+
+                    sqrt_area = np.sqrt(area)
+                    ratio = float(sqrt_area) / perimeter
+
+                    if ratio > 0.22:
+                        cv.drawContours(fields[i], [c], -1, (255, 0, 0), 1)
+                        circle = True
+                        break
+                    else:
+                        cv.drawContours(fields[i], [c], -1, (255, 0, 255), 1)
+
+                if circle:
+                    state.append('o')
+                else:
+                    state.append('x')
+
+    if len(state) != 0:
+        state = [state[:3], state[3:6], state[6:]]
+        return [cdstP], state
+    else:
+        return [cdstP], [['', '', ''], ['', '', ''], ['', '', '']]
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import cv2
+import numpy as np
 from skimage import io, transform
 from board import find_boards
 from game import find_game_state
@@ -5,18 +7,22 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 import glob
 
+original_images = []
 images = []
+res_i = 0
 
 
 def import_files(directory='*', file='*.jpg'):
     for file in glob.glob("../data/" + directory + "/" + file):
         image = io.imread(file)
         image = transform.resize(image, (250, 250))
-        images.append([image])
+        original_images.append([image, file])
+
+        img = transform.resize(image, (250, 250))
+        images.append([img])
 
 
 def show_images():
-
     max_len = 0
 
     for i in images:
@@ -28,26 +34,43 @@ def show_images():
         for j, img in enumerate(images[i]):
             plt.subplot(len(images), max_len, i * max_len + j + 1)
             plt.axis("off")
+            # plt.imshow(img, cmap=cm.gray, vmin=0., vmax=1.)
             plt.imshow(img, cmap=cm.gray, vmin=0., vmax=1.)
 
     plt.savefig('boards.pdf', bbox_inches='tight')
-    plt.show()
+    # plt.show()
 
 
-def show_result(game_states):
+def show_result(state):
+    image = np.ones((250, 250, 3), np.uint8) * 255
 
-    pass
+    y0, dy = 50, 25
+    text = ''
+    text += state[0][0] + '|' + state[0][1] + '|' + state[0][2] + "\n"
+    text += "____\n"
+    text += state[1][0] + '|' + state[1][1] + '|' + state[1][2] + "\n"
+    text += "____\n"
+    text += state[2][0] + '|' + state[2][1] + '|' + state[2][2] + "\n"
+    for j, line in enumerate(text.split('\n')):
+        y = y0 + j * dy
+        image = cv2.putText(image, line, (50, y), cv2.FONT_HERSHEY_PLAIN, 2, 2)
+
+    return image
 
 
 def calculate():
-    for image in images:
+    for image, original in zip(images, original_images):
         boards = find_boards(image[0])
         # image += boards
-        game_states = []
+        debug_images = []
+        states = []
         for board in boards:
-            game_states += find_game_state(board)
+                debug_image, state = find_game_state(board)
+                debug_images += debug_image
+                debug_images += [show_result(state)]
+                states.append(state)
 
-        image += game_states
+        image += debug_images
 
 
 if __name__ == "__main__":
